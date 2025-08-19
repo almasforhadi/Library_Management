6d4db512791240b1e3c1b1ac69cf236e575f06af
Almas-Forhadi <almasforhasi1999@gmail.com>
2025-08-19 22:21:19 +0600

    CSRF ORIGIN added

--- library/settings.py
+++ library/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+CSRF_TRUSTED_ORIGINS = ['https://library-management-lk7l.onrender.com','https://*.127.0.0.1']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
